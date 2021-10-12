753199dd39a1a3358a9e269a20bd01a6e7b81e17
psteeep <psteeep@users.noreply.github.com>
2021-10-12 22:54:20 +0300

    fixed 1 and 2 labs

--- lab1/iter_fix.py
+++ lab1/iter_fix.py
@@ -11,33 +11,45 @@ def fun(x):
 
 
 def g_fun(x):
-    pass
+    # return (math.sinh(x) - 12 * math.tanh(x) - 0.311)*math.e**(-x) + x
+    # return math.e ** (-x)
+    return x - fun(x) / fun1(x)
+
+
+def fun1(x):
+    """
+    derivative from main function
+    :param x: any argument
+    :return:
+    """
+    return math.cosh(x) - 12 * (1 / math.cosh(x) ** 2)
 
 
 def main():
     e = 10 ** (-4)
-    a = 0  # range 1
-    b = 1  # range 2
+    a = 3  # range 1
+    b = 4  # range 2
     x0 = (a + b) / 2
 
     F1 = fun(a)
     F2 = fun(b)
 
-    if F1 * F2 > 0:
+    if F1 * F2 < 0:
         itr = 1
         xp = x0
-        xn = fun(xp) # g_fun ???
+        xn = g_fun(xp)
         res = xn - xp
         xp = xn
-        while abs(res) < e:
+        while abs(res) > e:
             itr += 1
             xn = g_fun(xp)
             res = xn - xp
             xp = xn
-        print("res = ", xp)
-        print("itr =", itr)
+        print("Result of Fixed point method is  -> ", xp)
+        print("Number of iteration (posteriori ) is ->", itr)
+        print("prior =", (math.log10(abs(fun(x0) - x0) / e) / (math.log10(1) + 1)))
     else:
-        print("no solution")
+        print("No solution!")
 
 
 if __name__ == "__main__":
--- lab1/lab1_main.py
+++ lab1/lab1_main.py
@@ -1,22 +1,9 @@
-# sh(x) - 12th(x) - 0.311 = 0
-# eps = 0,0001
-
-# Метод простої ітерації
-
-# Метод Нютона
-
-# Апріорна та апостеріорна  оцінка кількості кроків
-
-# Поч. проміжок та поч. наближення однакове для обох методів
-
-# порівняти результати обох методів між собою
-
 import math
 
 e = 10 ** (-4)
 
-a = 0  # range 1
-b = 1 # range 2
+a = 3  # range 1
+b = 4  # range 2
 
 
 def fun(x):
@@ -37,45 +24,24 @@ def fun1(x):
     return math.cosh(x) - 12 * (1 / math.cosh(x) ** 2)
 
 
-def method_iterations(a1, b1):
-    try:
-        itr = 0
-        x1 = (a1 + b1) / 2  # seed
-        a1 = abs((fun(a1 + 0.0001) - fun(a1)) / 0.0001)
-        b1 = abs((fun(b1 + 0.0001) - fun(b1)) / 0.0001)
-
-        q = max(a1, b1)
-        q = (1 - q) / q
-        x0 = x1
-        x1 = fun(x0)
-        while abs(x1 - x0) <= (q*e):
-            itr += 1
-            x0 = x1
-            x1 = fun(x0)
-        return x1, itr
-    except ValueError:
-        print("Value not invalidate")
-
-
 def method_newton(a2, b2):
     try:
         itr = 0
-        x0 = (a2 + b2) / 2  # seed
+        x0 = (a2 + b2) / 2
         xn = fun(x0)
         xn1 = xn - fun(xn) / fun1(xn)
         while abs(xn1 - xn) > math.pow(10, -5):
             itr += 1
             xn = xn1
             xn1 = xn - fun(xn) / fun1(xn)
-        return xn1, itr
+        print("Result of Newton method is -> ", xn1)
+        print("Number of iteration is -> ", itr)
     except ValueError:
         print("Value not invalidate")
 
 
 def main():
-
-    print("Iterations method -> ", method_iterations(a, b))
-    print("Newton method -> ",  method_newton(a, b))
+    method_newton(a, b)
 
 
 if __name__ == "__main__":
